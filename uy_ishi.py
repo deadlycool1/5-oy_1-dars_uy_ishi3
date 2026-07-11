55c76606b2de8297bc5c2f5135381136a31d77cc
Shexroz <pennivays010909@gmail.com>
2026-07-11 19:37:08 +0500

    5-qadam: List funksiyalari qo'shildi

--- uy_ishi.py
+++ uy_ishi.py
@@ -62,3 +62,20 @@ def uzunligi(matn):
 
 
 
+def yigindi_list(lst):
+    return sum(lst)
+
+
+def eng_katta(lst):
+    return max(lst)
+
+
+def eng_kichik(lst):
+    return min(lst)
+
+
+def uzunligi_list(lst):
+    return len(lst)
+
+
+
