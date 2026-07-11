9d83db34fd207493483e39da6a354c159fd107df
Shexroz <pennivays010909@gmail.com>
2026-07-11 19:29:26 +0500

    2-qadam: Arifmetik funksiyalar qo'shildi

--- uy_ishi.py
+++ uy_ishi.py
@@ -9,3 +9,22 @@ def kvadrat(a):
 def kub(a):
     return a ** 3
 
+
+
+def yigindi(a, b):
+    return a + b
+
+
+def ayirma(a, b):
+    return a - b
+
+
+def kopaytma(a, b):
+    return a * b
+
+
+def bolish(a, b):
+    return a / b
+
+
+
